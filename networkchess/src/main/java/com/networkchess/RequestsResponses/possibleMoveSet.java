eeda591bed592c6de324766de13c21844cdc0381
Aidan <varanoab@merrimack.edu>
2024-04-24 09:55:45 -0400

    Basics Ideas for Json Communicatoin

---
+++ networkchess/src/main/java/com/networkchess/RequestsResponses/possibleMoveSet.java
@@ -0,0 +1,28 @@
+package com.networkchess.RequestsResponses;
+import java.io.InvalidObjectException;
+
+import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.types.JSONObject;
+import merrimackutil.json.types.JSONType;
+
+public class possibleMoveSet implements JSONSerializable {
+
+    @Override
+    public void deserialize(JSONType arg0) throws InvalidObjectException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+    }
+
+    @Override
+    public String serialize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'serialize'");
+    }
+
+    @Override
+    public JSONType toJSONType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toJSONType'");
+    }
+    
+}
