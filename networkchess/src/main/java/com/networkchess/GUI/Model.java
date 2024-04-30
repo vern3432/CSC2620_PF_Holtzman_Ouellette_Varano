79f1042c45669ec8e8c17f666c5d32aba1e10710
Seth H <holtzmans@merrimack.edu>
2024-04-30 12:36:08 -0400

    Doc: Added Javadocs and comments

--- networkchess/src/main/java/com/networkchess/GUI/Model.java
+++ networkchess/src/main/java/com/networkchess/GUI/Model.java
@@ -4,10 +4,26 @@ import com.networkchess.GameLogic.Board;
 
 import javax.swing.*;
 
+/**
+ * Model is our internal representation of the chess game which deals with updating the board and communicating with the server
+ * Model should run on a separate thread so it can wait for server updates
+ */
 public class Model extends JPanel implements Runnable {
+    /**
+     * Board object which has all of our peices
+     */
     private Board board = new Board();
+    /**
+     * Board label us text of board for testing
+     */
     JTextArea boardLabel = new JTextArea();
+
+    /**
+     * Creates model for internal representation of chess game
+     * @param name - name given to game
+     */
     public Model(String name) {
+        //adds board object toString as a placeholder
         this.add(boardLabel);
 
         boardLabel.setText(board.toString());
@@ -16,6 +32,6 @@ public class Model extends JPanel implements Runnable {
 
     @Override
     public void run() {
-
+        //TO:DO implement run method and threads
     }
 }
