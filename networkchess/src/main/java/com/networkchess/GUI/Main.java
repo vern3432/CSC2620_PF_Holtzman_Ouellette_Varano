63012dc925c0db688b3c813dc247bd072ea357a3
Seth H <holtzmans@merrimack.edu>
2024-04-30 12:17:18 -0400

    Feat: Added preliminary MVC

---
+++ networkchess/src/main/java/com/networkchess/GUI/Main.java
@@ -0,0 +1,15 @@
+package com.networkchess.GUI;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ViewController gui = new ViewController();
+        // What to do when the window closes:
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Size of the window, in pixels
+        gui.setSize(800,800);
+        // Make the window "visible"
+        gui.setVisible(true);
+    }
+}
