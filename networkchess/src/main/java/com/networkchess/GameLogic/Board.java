53993dca66202704c582d0651d65a3642eef894c
SOuellette22 <143441012+SOuellette22@users.noreply.github.com>
2024-05-08 18:20:06 -0400

    fix: black pawns can now promote

--- networkchess/src/main/java/com/networkchess/GameLogic/Board.java
+++ networkchess/src/main/java/com/networkchess/GameLogic/Board.java
@@ -207,7 +207,7 @@ public class Board {
                 Integer ny = Integer.valueOf(nxy[1]) - 1;
 
                 gameBoard[px][py] = null;
-                if (piece instanceof Pawn && ny == 8) {
+                if (piece instanceof Pawn && (piece.getColor().equals("white") && ny == 8) || (piece.getColor().equals("black") && ny == 1)) {
                     if (checkCheck(piece.getColor())) {
                         JOptionPane.showMessageDialog(new JPanel(), "That is not a possible move. You will still/be in check.");
                         gameBoard[px][py] = piece;
