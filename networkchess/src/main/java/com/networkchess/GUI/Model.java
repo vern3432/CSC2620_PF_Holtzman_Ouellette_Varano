f75279b681636e7f22da57e1ff4995ce69c343a7
SOuellette22 <143441012+SOuellette22@users.noreply.github.com>
2024-05-08 20:15:52 -0400

    remove test syso

--- networkchess/src/main/java/com/networkchess/GUI/Model.java
+++ networkchess/src/main/java/com/networkchess/GUI/Model.java
@@ -464,7 +464,6 @@ public class Model extends JPanel implements Runnable {
             int newY = Integer.parseInt(parts[1]);
 
             if (board.updateBoard(selectedPiece, newX + ";" + newY)) {
-                System.out.println(selectedPiece);
                 sendMoveToServer(newX, newY, pieceX, pieceY);
                 this.isTurn = false;
                 updateGame();
